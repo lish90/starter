a0d3cbd1401f4eeb3f29c73cb971d7c61cb069aa
alisha musamabik <alisha.musamabik@gmail.com>
2020-04-20 20:23:46 +0100

    restarting Javascript challenge and adding features

---
+++ main.js
@@ -0,0 +1,25 @@
+const marksMass = 86;
+const marksHeight = 1.59;
+
+const johnsMass = 82;
+const johnsHeight = 1.78;
+
+
+let markBMI = marksMass / (marksHeight * marksHeight); 
+let johnsBMI = johnsMass /(johnsHeight *johnsHeight);
+console.log(markBMI);
+console.log(marksMass);
+console.log(johnsBMI);
+if (markBMI > johnsBMI) {
+    console.log("Is Mark's BMI higher than John's?");    
+} else {
+    console.log("Is john's BMI higher than Mark's?");  
+}
+
+
+
+//  mass / height^2 = mass / (height * height). 
+// (mass in kg and height in meter)
+// var BMIMark = massMark / (heightMark * heightMark);
+
+    
