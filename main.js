8332bed9cbdae13007a4766d8649ca2a6b0690ed
alisha musamabik <alisha.musamabik@gmail.com>
2020-04-22 16:38:04 +0100

    Javascript challenge and adding answer on HTML

--- main.js
+++ main.js
@@ -4,28 +4,42 @@ const marksHeight = 1.59;
 const johnsMass = 82;
 const johnsHeight = 1.78;
 
-
+// calcuation of the BMI 
 let markBMI = marksMass / (marksHeight * marksHeight);
 let johnsBMI = johnsMass / (johnsHeight * johnsHeight);
-console.log(markBMI);
-console.log(marksMass);
-console.log(johnsBMI);
+console.log(markBMI, johnsBMI);
+let marksHigherBMI = markBMI > johnsBMI;
+console.log(marksHigherBMI);
+
+
 if (markBMI > johnsBMI) {
-    console.log("Is Mark's BMI higher than John's?");
+    console.log("Is Mark's BMI higher than John's? " + marksHigherBMI);
 } else {
-    console.log("Is john's BMI higher than Mark's?");
+    console.log("Is john's BMI higher than Mark's? " + marksHigherBMI);
 }
 
 
+// let grabbingTheID = document.getElementById('challenge_answer'); 
+// let inputPTag = document.createElement('div');
 
-embedCode = () =>{
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      document.body.insertAdjacentHTML("beforeend", data);
-    });
-  };
+// inputPTag.innerHTML = 'Is Mark\'s BMI higher than John\'s? + marksHigherBMI ';
 
-//  mass / height^2 = mass / (height * height). 
-// (mass in kg and height in meter)
-// var BMIMark = massMark / (heightMark * heightMark);
+// grabbingTheID.insertAdjacentHTML('afterend', '<div id="answer_input">"Is Mark\'s BMI higher than John\'s?  + marksHigherBMI </div>');
+
+// grabbingTheID.insertAdjacentHTML('afterend', inputPTag);
+
+
+
+const grabbingTheID = document.querySelector('#challenge_answer');
+const inputPTag = document.createElement('p');
+
+inputPTag.innerText = 'Is Mark\'s BMI higher than John\'s? ' + marksHigherBMI.toString();
+grabbingTheID.insertAdjacentHTML('beforeend', inputPTag.outerHTML);
+
+
+//     fetch(index.js)
+//     .then(response => response.text())
+//     .then(data => {
+//       document.body.insertAdjacentHTML("afterend", data);
+//     });
+
